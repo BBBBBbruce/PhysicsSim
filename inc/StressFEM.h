41a2cce4ce73b3326f227a5cf572f5aec37d58ec
Zhiyuan Zhang <bubblebruce@outlook.com>
2022-02-01 12:43:44 +0000

    setup StressFEM class

--- inc/StressFEM.h
+++ inc/StressFEM.h
@@ -1,7 +1,26 @@
+#ifndef STRESSFEM_H
+#define STRESSFEM_H
 #pragma once
+
+
 #include "Engine.h"
+
+
 class StressFEM :
     public Engine
 {
+
+public:
+    StressFEM();
+    StressFEM(string t_path);
+    void run(float time, int seq);// in seconds
+    bool collision_detection();
+    vector<StaticObj> getStaticObjs();
+    vector<DynamicObj> getDynamicObjs();
+    void load_scene(int pre_seq);
+    void save_scene(int seq);
+    void InitConfigs(string targetpath, json currentconfig);
+
 };
 
+#endif // !STRESSFEM
--- src/StressFEM.cpp
+++ src/StressFEM.cpp
@@ -1,1 +1,47 @@
 #include "StressFEM.h"
+
+
+StressFEM::StressFEM()
+{
+}
+
+StressFEM::StressFEM(string t_path)
+{
+    vector<StaticObj>StaticVec;
+    vector<DynamicObj>DynamicVec;
+    this->tpath = t_path;
+}
+
+void StressFEM::run(float time, int seq)
+{
+    // Implement the physics
+}
+
+bool StressFEM::collision_detection()
+{
+    // implement CD
+    return false;
+}
+
+vector<StaticObj> StressFEM::getStaticObjs()
+{
+    return StaticVec;
+}
+
+vector<DynamicObj> StressFEM::getDynamicObjs()
+{
+    return DynamicVec;
+}
+
+void StressFEM::load_scene(int pre_seq)
+{
+}
+
+void StressFEM::save_scene(int seq)
+{
+}
+
+void StressFEM::InitConfigs(string targetpath, json currentconfig)
+{
+
+}
